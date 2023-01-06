96790b9cf7dacf6da4dcc63a3157a3e79c8e6cf5
MaciejkaG <maciejkadev@protonmail.com>
2023-01-06 17:21:59 +0100

    redesigned website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const ip = "mc.playgcraft.pl"
+const ip = "mc.hypixel.net"
 document.getElementById("ip").innerHTML = `IP: ${ip}<i class="fas fa-copy"></i>`;
 updateStats();
 setInterval(updateStats, 1000);
@@ -33,4 +33,37 @@ async function fetchWithTimeout(resource, options = {}) {
     clearTimeout(id);
     clearTimeout(id2);
     return response;
+}
+
+document.getElementById("switch").addEventListener("change", function () {
+    if (document.getElementById("switch").checked == true) {
+        setCookie("darkTheme", 1);
+        document.getElementsByTagName("body")[0].setAttribute("style", "animation: darkTheme 0.5s 1; color: white; background-color: #000000;");
+    } else {
+        setCookie("darkTheme", 0);
+        document.getElementsByTagName("body")[0].setAttribute("style", "animation: lightTheme 0.5s 1; color: black;");
+        let bgAnimation = setTimeout(() => {
+            document.getElementsByTagName("body")[0].setAttribute("style", "animation: bg-animation 10s ease-in-out infinite;");
+        }, 500);
+    }
+});
+if (getCookie("darkTheme") == 1) {
+    document.getElementById("switch").checked = true;
+    document.getElementsByTagName("body")[0].setAttribute("style", "animation: darkTheme 0.1s 1; color: white; background-color: #000000;");
+    setCookie("darkTheme", 1);
+}
+else {
+    document.getElementById("switch").checked = false;
+    document.getElementsByTagName("body")[0].setAttribute("style", "animation: bg-animation 10s ease-in-out infinite; color: black;");
+    setCookie("darkTheme", 0);
+}
+
+function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString() + "; path=/";
+}
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
 }
