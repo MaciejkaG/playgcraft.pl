bd06037e5aa19ca20769c5a8fb6f971b3a6c6367
MaciejkaG <maciejkadev@protonmail.com>
2023-01-06 11:45:37 +0100

    added update time display

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function updateStats() {
                 status = '<span class="green">online</span>'
             }
             document.getElementById("status").innerHTML = `Status: ${status}`;
+            var update = Math.round(60 - (new Date().getSeconds() - json.debug.cacheexpire))
+            document.getElementById("update").innerHTML = `Update za: ${update}s`;
             if (json.online) {
                 document.getElementById("players").innerHTML = `Gracze: ${json.players.online}/${json.players.max}`;
             } else {
