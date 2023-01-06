a361c9daad432c67a2674a6f55c0ff86b02bbcf5
MaciejkaG <maciejkadev@protonmail.com>
2023-01-06 11:49:39 +0100

    temporarily removed update display because of problems

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const ip = "mc.playgcraft.pl"
+const ip = "kokscraft.pl"
 document.getElementById("ip").innerHTML = `IP: ${ip}<i class="fas fa-copy"></i>`;
 updateStats();
 setInterval(updateStats, 1000);
@@ -14,8 +14,8 @@ function updateStats() {
                 status = '<span class="green">online</span>'
             }
             document.getElementById("status").innerHTML = `Status: ${status}`;
-            var update = Math.round(60 - (new Date().getSeconds() - json.debug.cacheexpire))
-            document.getElementById("update").innerHTML = `Update za: ${update}s`;
+            // var update = Math.round(60 - (new Date().getSeconds() - json.debug.cacheexpire))
+            // document.getElementById("update").innerHTML = `Update za: ${update}s`;
             if (json.online) {
                 document.getElementById("players").innerHTML = `Gracze: ${json.players.online}/${json.players.max}`;
             } else {
