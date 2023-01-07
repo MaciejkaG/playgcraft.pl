e36254f4100a57bc5b0c145b3c7adf8f4bbab2ad
MaciejkaG <maciejkadev@protonmail.com>
2023-01-07 23:12:09 +0100

    ostatnie poprawki

--- assets/js/lepszyplayercounter.js
+++ assets/js/lepszyplayercounter.js
@@ -1,4 +1,9 @@
 const ip = "mc.playgcraft.pl"
+document.getElementById("onlinedot").style.animation = "";
+document.getElementById("onlinedot").style.background = "rgb(255, 73, 73)";
+document.getElementById("onlinetext").innerHTML = "offline";
+document.getElementById("onlinetext").style.color = "rgb(255, 73, 73)";
+document.getElementById("playercount").innerHTML = `-`;
 updateStats();
 setInterval(updateStats, 1000);
 
@@ -34,19 +39,19 @@ async function fetchWithTimeout(resource, options = {}) {
     clearTimeout(id2);
     return response;
 }
-function copyIP() {
-    try {
+const unsecuredCopyToClipboard = (text) => { const textArea = document.createElement("textarea"); textArea.value = text; document.body.appendChild(textArea);  textArea.select(); try { document.execCommand('copy') } catch (err) { console.error('Unable to copy to clipboard', err) } document.body.removeChild(textArea) };
+const copyIP = str => {
+    if (window.isSecureContext && navigator.clipboard) {
         navigator.clipboard.writeText(ip);
-    } catch (err) {
-        console.error('Błąd kopiowania IP: ', err);
-    } finally {
-        document.getElementById("iptooltiptext").style.color = "rgb(0, 102, 255)";
-        document.getElementById("iptooltiptext").style.bottom = "-50%";
-        document.getElementById("iptooltiptext").innerHTML = "Skopiowano IP!";
-        setTimeout(() => {
-            document.getElementById("iptooltiptext").style.color = "rgb(255, 255, 255)";
-            document.getElementById("iptooltiptext").style.bottom = "-100%";
-            document.getElementById("iptooltiptext").innerHTML = "Kliknij aby skopiować!";
-        }, 2000);
+    } else {
+        unsecuredCopyToClipboard(ip);
     }
-}
+    document.getElementById("iptooltiptext").style.color = "rgb(0, 102, 255)";
+    document.getElementById("iptooltiptext").style.bottom = "-50%";
+    document.getElementById("iptooltiptext").innerHTML = "Skopiowano IP!";
+    setTimeout(() => {
+        document.getElementById("iptooltiptext").style.color = "rgb(255, 255, 255)";
+        document.getElementById("iptooltiptext").style.bottom = "-100%";
+        document.getElementById("iptooltiptext").innerHTML = "Kliknij aby skopiować!";
+    }, 2000);
+};
